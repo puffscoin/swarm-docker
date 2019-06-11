7de2f3590df3507c521928f9b2c0b8f72ae892ce
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-06-11 13:27:19 -0300

    Update build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
-docker build -t "ethdevops/swarm:$DOCKER_TAG" --target swarm .
-docker build -t "ethdevops/swarm-smoke:$DOCKER_TAG" --target swarm-smoke .
-docker build -t "ethdevops/swarm-global-store:$DOCKER_TAG" --target swarm-global-store .
+docker build -t "puffsdevops/swarm:$DOCKER_TAG" --target swarm .
+docker build -t "puffsdevops/swarm-smoke:$DOCKER_TAG" --target swarm-smoke .
+docker build -t "puffsdevops/swarm-global-store:$DOCKER_TAG" --target swarm-global-store .
